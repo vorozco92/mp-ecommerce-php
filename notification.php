fa3cf03483e76c0da7cb5943997388041b4f3770
vorozco92 <vorozco@ebcomm.mx>
2020-08-03 14:13:42 -0500

    Add files via upload

--- notification.php
+++ notification.php
@@ -1,16 +1,26 @@
 <?php
 
+	//include('class/vendor/autoload.php');
+	//include('config/configuration_payment.php');
 	error_log(date('H:i:s ') . getenv('REMOTE_ADDR') . " Notificacion \n", 3, 'log/notification.log');
-	//$json_event = file_get_contents('php://input', true);
+	$json_data = file_get_contents('php://input', true);
+	error_log(date('H:i:s ') . getenv('REMOTE_ADDR') . " json data:".$json_data."\n", 3, 'log/notification.log');
 	error_log(date('H:i:s ') . getenv('REMOTE_ADDR') . " GET code:".$_GET['code']."\n", 3, 'log/notification.log');
 	foreach($_POST as $key => $value){
-		error_log(date('H:i:s ') . getenv('REMOTE_ADDR') . $key." ".$value."\n", 3, 'log/notification.log');
+		error_log(date('H:i:s ') . getenv('REMOTE_ADDR') ."POST ". $key." ".$value."\n", 3, 'log/notification.log');
 	}
 
 	foreach($_GET as $key => $value){
-		error_log(date('H:i:s ') . getenv('REMOTE_ADDR') . "GET ".$key." ".$value."\n", 3, 'log/notification.log');
+		error_log(date('H:i:s ') . getenv('REMOTE_ADDR') . "GET data: ".$key." ".$value."\n", 3, 'log/notification.log');
 	}	
-	//$message_not = json_decode($json_event);		
-	//header('HTTP/1.1 200 OK');
-	//exit;
+	
+	/*\MercadoPago\SDK::setAccessToken($config_mp['access_token']);
+	$id = ($_GET["id"]);
+
+    switch($_GET["type"]) {
+        case "payment":
+            $payment = \MercadoPago\Payment::find_by_id($id);
+				echo $payment;
+            break;
+    }*/
 ?>
