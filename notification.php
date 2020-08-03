f1474534638dd41d3e9422317cad74e8084d56c0
vorozco92 <vorozco@ebcomm.mx>
2020-08-03 00:12:48 -0500

    Add files via upload

--- notification.php
+++ notification.php
@@ -2,7 +2,7 @@
 
 	$json_event = file_get_contents('php://input', true);
 	$message_not = json_decode($json_event);	
-	error_log(date('H:i:s ') . getenv('REMOTE_ADDR') . " $message_not\n", 3, 'log/notification.log');
+	error_log(date('H:i:s ') . getenv('REMOTE_ADDR') . " $message_not\n", 3, 'log/notification_'.date('Ymd').'.log');
 	header('HTTP/1.1 200 OK');
 	exit;
 ?>
